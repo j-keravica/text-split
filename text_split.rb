be64a44e0da3b89b1803b1ca7a7170ba47466867
j-keravica <jelena.keravica@gmail.com>
2016-08-18 22:09:37 +0200

    Fix indentation

--- text_split.rb
+++ text_split.rb
@@ -2,9 +2,8 @@ require "./lib/page"
 
 class TextSplit < Sinatra::Base
 
-	post "/split", :provides => :json do
-	  url = params[:url]
-    # elements = params[:elements]
+  post "/split", :provides => :json do
+    url = params[:url]
 
     extracted_page = Page.extract(url)
 
@@ -12,6 +11,6 @@ class TextSplit < Sinatra::Base
       :title => extracted_page.title,
       :text => extracted_page.text
     })
-	end
+  end
 
 end
