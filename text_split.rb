7a4c91dbc4cb38a5bcb626d3ce09c4f1ac19e60a
j-keravica <jelena.keravica@gmail.com>
2016-08-30 23:22:28 +0200

    Add error handling

--- text_split.rb
+++ text_split.rb
@@ -1,16 +1,22 @@
 require "./lib/page"
+require "json"
 
 class TextSplit < Sinatra::Base
 
   post "/split", :provides => :json do
     url = params[:url]
 
-    extracted_page = Page.extract(url)
+    begin
+      extracted_page = Page.extract(url)
 
-    json({
-      :title => extracted_page.title,
-      :text => extracted_page.text
-    })
+      content_type :json
+      json({
+        :title => extracted_page.title,
+        :text => extracted_page.text
+      })
+    rescue Errno::ENOENT, OpenURI::HTTPError
+      halt 404
+    end
   end
 
 end
