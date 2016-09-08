3b88001b4574c3c0a38019a550bf51956ef33c47
j-keravica <jelena.keravica@gmail.com>
2016-09-09 00:52:42 +0200

    Add custom content extraction

--- spec/lib/page_spec.rb
+++ spec/lib/page_spec.rb
@@ -3,31 +3,47 @@ require "./lib/page"
 
 RSpec.describe Page do
 
-  subject { described_class.new(File.join(Sinatra::Application.settings.root, "/spec/lib/example.html")) }
+  let(:web_page) { File.join(Sinatra::Application.settings.root, "/spec/lib/example.html") }
 
-  describe "#text" do
-    it "returns web page text" do
-      expect(subject.text).to include("okay to be angry and to be kind of dark")
+  describe "#title" do
+    before do
+      @page = described_class.new(web_page, "")
+    end
+
+    it "returns web page title" do
+      expect(@page.title).to include("Stop Trying To Inspire Me")
     end
   end
 
-  describe "#title" do
-    it "returns web page title" do
-      expect(subject.title).to include("Stop Trying To Inspire Me")
+  describe "#content" do
+    before do
+      @page = described_class.new(web_page, "")
+    end
+
+    it "returns web page text" do
+      expect(@page.content).to include("okay to be angry and to be kind of dark")
     end
   end
 
   describe "#custom_content" do
     context "valid xpath" do
+      before do
+        @page = described_class.new(web_page, "//p[@name='2201']")
+      end
+
       it "returns content specified by user's xpath" do
-        parsed_content = subject.custom_content("//p[@name='2201']")
+        parsed_content =@page.custom_content
         expect(parsed_content).to include("Everything happens for a reason!")
       end
     end
 
     context "invalid xpath" do
+      before do
+        @page = described_class.new(web_page, "troll")
+      end
+
       it "raises an invalid path error" do
-        expect { subject.custom_content("troll") }.to raise_error(Exceptions::InvalidPath)
+        expect { @page.custom_content }.to raise_error(Exceptions::InvalidPath)
       end
     end
   end
